5d86ec8f2cae60333f0f41db7dd4cf3286a301a6
vivianpsc <77083803+vivianpsc@users.noreply.github.com>
2021-01-07 00:07:55 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,11 @@
+#calculadora
+
+primeiro_numero = float(input ('Digite o primeiro número: '))
+segundo_numero = float(input ('Digite o segundo número: '))
+
+soma = primeiro_numero + segundo_numero
+subtracao = primeiro_numero - segundo_numero
+multiplicacao = primeiro_numero * segundo_numero
+divisao = primeiro_numero / segundo_numero
+
+print(f'A soma é: {soma},  a Subtração é: {subtracao},  a Divisão é: {divisao} e a multiplicação é: {multiplicacao} ' )
