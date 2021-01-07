553ef0292fe1a64cec974ef4bb349947c210036b
vivianpsc <77083803+vivianpsc@users.noreply.github.com>
2021-01-06 22:28:12 -0300

    Add files via upload

---
+++ Calculadora.py
@@ -0,0 +1,13 @@
+notas = [10, 8,7, 5,9]
+
+quantidade = len(notas)
+print('Quantidade de Notas', quantidade)
+
+soma = sum(notas)
+
+
+print('A soma vale', soma)
+
+media = soma /quantidade
+
+print('A media vale',media)
